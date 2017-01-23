73769ecd0d383e514122242bde5f3b4290207cac
July Tikhonov <july.tikh@gmail.com>
2017-01-23 15:36:24 +0300

    fix firefox and improve scrolling on some browsers (probably)

--- static/metatable.js
+++ static/metatable.js
@@ -3,12 +3,14 @@ function create_metatable($table) { // {{{
   $table.$head = $table.children('thead').first();
   $table.$body = $table.children('tbody').first();
   function create_metacell() {
-    var $metacell =  $('<div />').addClass('metacell')
+    var $metacell =  $('<td />').addClass('metacell')
       .addClass('ritz'); // "ritz" is expected by google CSS
     $metacell.$scroller = $('<div />').addClass('scroller')
       .appendTo($metacell);
-    $metacell.$pager = $('<div />').addClass('pager')
+    $metacell.$overlay = $('<div />').addClass('overlay')
       .appendTo($metacell.$scroller);
+    $metacell.$pager = $('<div />').addClass('pager')
+      .appendTo($metacell);
     $metacell.$table = $($table[0].cloneNode())
       .appendTo($metacell.$pager);
     $metacell.$table.$head = $($table.$head[0].cloneNode())
@@ -24,8 +26,6 @@ function create_metatable($table) { // {{{
   var $AA = $('<table />').addClass('metatable')
     .append( $('<tr />').append($FF, $FM), $('<tr />').append($MF, $MM) );
 
-  const frozenborder = 3; /* sync with metatable.css */
-
   { // clone head row {{{
     var $headrow = $table.$head.children('tr').first();
     var $corner = $headrow.children('th:first-child');
@@ -36,6 +36,7 @@ function create_metatable($table) { // {{{
       $headers$moving = $header$frozen.nextAll();
     } else {
       $headers$moving = $corner.nextAll();
+      $AA.addClass('no-frozen-columns');
     }
     $headrow$moving.append($headers$moving);
     $header$frozen.remove();
@@ -53,14 +54,8 @@ function create_metatable($table) { // {{{
       $rows$moving = $row$frozen.nextAll('tr');
     } else {
       $rows$moving = $table.$body.children('tr');
+      $AA.addClass('no-frozen-rows');
     }
-    $rows$moving.each(function() {
-      split_row($(this), $MF.$table.$body, $MM.$table.$body);
-    });
-    $row$frozen.remove()
-    $table.$body.children('tr').each(function() {
-      split_row($(this), $FF.$table.$body, $FM.$table.$body);
-    });
     function split_row($row, $Fbody, $Mbody) {
       var $cell$frozen = $row.children('td.freezebar-cell').first();
       var $cells$moving;
@@ -76,41 +71,66 @@ function create_metatable($table) { // {{{
       $Fbody.append($row);
       $Mbody.append($row$moving);
     }
+    $rows$moving.each(function() {
+      split_row($(this), $MF.$table.$body, $MM.$table.$body);
+    });
+    $row$frozen.remove()
+    $table.$body.children('tr').each(function() {
+      split_row($(this), $FF.$table.$body, $FM.$table.$body);
+    });
   } // }}}
 
   $AA.resize = function() {
-    var frozenwidth = $FF.$table.width();
-    var frozenheight = $FF.$table.height()
-    var viewwidth = $(window).width();
-    var viewheight = $(window).height();
-    var displayfrozenwidth = Math.min(frozenwidth, viewwidth / 2.0);
-    var displayfrozenheight = Math.min(frozenheight, viewheight / 2.0);
-    var displaymainwidth = viewwidth - Math.min(frozenwidth, viewwidth / 2.0) - frozenborder;
-    var displaymainheight = viewheight - Math.min(frozenheight, viewheight / 2.0) - frozenborder;
-    $FF.$scroller.css({width: displayfrozenwidth, height: displayfrozenheight});
-    $FM.$scroller.css({width: displaymainwidth,   height: displayfrozenheight});
-    $MF.$scroller.css({width: displayfrozenwidth, height: displaymainheight});
-    $MM.$scroller.css({width: displaymainwidth,   height: displaymainheight});
+    var dA = {
+      width:  $(window).width(),
+      height: $(window).height() };
+    var b = {
+      width:  parseInt($FF.css('border-right-width'),  10),
+      height: parseInt($FF.css('border-bottom-width'), 10) };
+    var F = {
+      width:  Math.max($FF.$table.width(),  $MF.$table.width()),
+      height: Math.max($FF.$table.height(), $FM.$table.height()) };
+    var M = {
+      width:  Math.max($FM.$table.width(),  $MM.$table.width()),
+      height: Math.max($MF.$table.height(), $MM.$table.height()) };
+    var dF = {
+      width:  Math.min(F.width,  dA.width  / 2.0),
+      height: Math.min(F.height, dA.height / 2.0) };
+    var dM = {
+      width:  dA.width  - dF.width  - b.width,
+      height: dA.height - dF.height - b.height };
+    $FF.$scroller.css({width: dF.width, height: dF.height});
+    $FM.$scroller.css({width: dM.width, height: dF.height});
+    $MF.$scroller.css({width: dF.width, height: dM.height});
+    $MM.$scroller.css({width: dM.width, height: dM.height});
+    $FF.$overlay.css({width: F.width, height: F.height});
+    $FM.$overlay.css({width: M.width, height: F.height});
+    $MF.$overlay.css({width: F.width, height: M.height});
+    $MM.$overlay.css({width: M.width, height: M.height});
   }
   $(window).resize($AA.resize);
 
   $MM.$scroller.scroll(function() {
-    var MM_table_position = $MM.$table.position();
-    var MM_scroller_position = $MM.$scroller.position();
-    $MF.$table.css({'top' : MM_table_position.top - MM_scroller_position.top});
-    $FM.$table.css({'left' : MM_table_position.left - MM_scroller_position.left});
+    var position = {
+      left: $MM.$scroller.scrollLeft(),
+      top:  $MM.$scroller.scrollTop() };
+    $MM.$table.css({'left' : -position.left, 'top'  : -position.top});
+    $MF.$table.css({'top'  : -position.top});
+    $FM.$table.css({'left' : -position.left});
   });
 
   $MF.$scroller.scroll(function() {
-    var MF_table_position = $MF.$table.position();
-    var MF_scroller_position = $MF.$scroller.position();
-    $FF.$table.css({'left' : MF_table_position.left - MF_scroller_position.left});
+    var position = {
+      left: $MF.$scroller.scrollLeft() };
+    $MF.$table.css({'left' : -position.left});
+    $FF.$table.css({'left' : -position.left});
   });
 
   $FM.$scroller.scroll(function() {
-    var FM_table_position = $FM.$table.position();
-    var FM_scroller_position = $FM.$scroller.position();
-    $FF.$table.css({'top' : FM_table_position.top - FM_scroller_position.top});
+    var position = {
+      top: $FM.$scroller.scrollTop() };
+    $FM.$table.css({'top' : -position.top});
+    $FF.$table.css({'top' : -position.top});
   });
 
   return $AA;
